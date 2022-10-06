fbcc815bf2d15b0e68c0ed95b4399e32ed038a7a
Dymond <dymond.hoffman03@gmail.com>
2022-10-06 15:06:03 -0700

    fixed errs

--- models/Product.js
+++ models/Product.js
@@ -21,7 +21,7 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10,2),
       allowNull: false,
       validate: {
         isDecimal: true,
@@ -36,7 +36,7 @@ Product.init(
       }
     },
     category_id: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       references: {
         model: 'category',
         key: 'id',
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -14,14 +14,14 @@ ProductTag.init(
       autoIncrement: true,
     },
     product_id: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       references: {
         model: 'product',
         key: 'id',
       }
     },
     tag_id: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       references: {
         model: 'tag',
         key: 'id',
